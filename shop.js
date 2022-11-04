516ed8a3eba033a712d1bbd1d78677060a3e7a46
jayhun0521 <jayhun21@gmail.com>
2022-11-03 21:29:42 -0400

    updating html, css, and js files

--- shop.js
+++ shop.js
@@ -1,11 +1,64 @@
 var shop = [
     {
       title: 'Tigers',
-      image: './img/gerbile.jpg',
-      price: '$7,500-15,000'
-      description
+      image: 'https://upload.wikimedia.org/wikipedia/commons/3/3f/Walking_tiger_female.jpg',
+      price: '$7,500-15,000',
+      description: "lorem ipsum"
     },
-    
+    {
+      title: "Rocky-Rottweiler",
+      image: 'http://127.0.0.1:5500/petShopProject/rottweiler-2160308_1280-1024x682.jpg',
+      price: '2500.00',
+      description: ""
+    },
+    {
+      title: "Heathcliff- Persian Tabby",
+      image: 'http://127.0.0.1:5500/petShopProject/heathcliff1.jpg',
+      price: '50.00',
+      description: ''
+    },
+    {
+      title: "Rex- Chinchilla",
+      image: 'http://127.0.0.1:5500/petShopProject/chinchilla.jpg',
+      price: '4000.00',
+      description: ''
+    },
+    {
+      title: "Chiquita- Sugar Glider",
+      image: 'http://127.0.0.1:5500/petShopProject/Flying-Squirrel-3.jpg',
+      price: '5500.00',
+      description: ''
+    },
+    {
+      title: "Lobo- Iguana",
+      image: 'http://127.0.0.1:5500/petShopProject/iguana.jpg',
+      price: '1500.00',
+      description: ''
+    },
+    {
+      title: "Billy- Capushin Monkey",
+      image:  'http://127.0.0.1:5500/petShopProject/capuchin.jpg',
+      price: '10,000.00',
+      description: ''
+    },
+    {
+      title: "Mojo- English Bulldog",
+      image: 'http://127.0.0.1:5500/petShopProject/english_bulldog.jpg',
+      price: '5000.00',
+      description: ''
+    },
+    {
+      title: "Broly- Pit Bull",
+      image: 'http://127.0.0.1:5500/petShopProject/Pitbull-puppies-for-sale-in-California-862x861.jpg',
+      price: '1800.00',
+      description: ''
+    },
+    {
+      title: "Billy- Cockatoo",
+      image: 'http://127.0.0.1:5500/petShopProject/cockatoo.jpg',
+      price: '2000.00',
+      description: ''
+    },
   
     
     ]
@@ -14,37 +67,16 @@ var shop = [
   
   
   
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
+
     
   
   
   
-    for (var i=0; i < 0 ; i++){
+    for (var i=0; i < shop.length; i++){
         var heading = '<div class="product ' + '"><span><h5>' + shop[i].title + '</h5>'
-        var image = '<img src="' + shop[i] + '"/'
-        var price = '<p> $' + .price + '</p></span>'
-        var description = '<div class=""><p>'+ .description + '</p><button type="button" class="btn btn-warning"> add to cart</button></div></div>'
+        var image = '<img src="' + shop[i].image + '"/'
+        var price = '<p> $' + shop[i].price + '</p></span>'
+        var description = '<div class=""><p>'+ shop[i].description + '</p><button type="button" class="btn btn-warning"> add to cart</button></div></div>'
         var concatThis = heading + image + price + description;
         postHTML = postHTML + concatThis
     }
